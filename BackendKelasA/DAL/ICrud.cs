fd325f53317fa968f721dbab767762e8a9128046
ekurniawan <erick.kurniawan@gmail.com>
2018-11-19 09:52:35 +0700

    menambahkan interface ICrud

---
+++ BackendKelasA/DAL/ICrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendKelasA.DAL
+{
+    public interface ICrud<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(string id);
+        Task Insert(T obj);
+        Task Update(T obj);
+        Task Delete(T obj);
+    }
+}
