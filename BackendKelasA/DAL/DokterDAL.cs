5ea302054f92087270598e0f35cd216079aab01a
ekurniawan <erick.kurniawan@gmail.com>
2018-11-19 10:23:09 +0700

    revisi IDokter

--- BackendKelasA/DAL/DokterDAL.cs
+++ BackendKelasA/DAL/DokterDAL.cs
@@ -3,32 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
+using System.Data.SqlClient;
+using BackendKelasA.Models;
 
 namespace BackendKelasA.DAL
 {
     public class DokterDAL : IDokter
     {
-        public Task Delete(IDokter obj)
+        private IHelper _helper;
+        public DokterDAL(IHelper helper)
+        {
+            _helper = helper;
+        }
+
+        public Task Delete(Dokter obj)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<IDokter>> GetAll()
+        public async Task<IEnumerable<Dokter>> GetAll()
+        {
+            using(SqlConnection conn = new SqlConnection(_helper.GetConnectionString()))
+            {
+                string strSql = @"select * from Dokter order by Username asc";
+                var results = await conn.QueryAsync<Dokter>(strSql);
+                return results;
+            }
+        }
+
+        public Task<Dokter> GetById(string id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IDokter> GetById(string id)
+        public Task Insert(Dokter obj)
         {
             throw new NotImplementedException();
         }
 
-        public Task Insert(IDokter obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Update(IDokter obj)
+        public Task Update(Dokter obj)
         {
             throw new NotImplementedException();
         }
--- BackendKelasA/DAL/IDokter.cs
+++ BackendKelasA/DAL/IDokter.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using BackendKelasA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BackendKelasA.DAL
 {
-    public interface IDokter : ICrud<IDokter>
+    public interface IDokter : ICrud<Dokter>
     {
     }
 }
