7090592f986d6c2b2feb9c5993492125c0f3b708
ekurniawan <erick.kurniawan@gmail.com>
2018-11-19 09:38:34 +0700

    first commit

---
+++ BackendKelasA/Models/Dokter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendKelasA.Models
+{
+    public class Dokter
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Alamat { get; set; }
+        public string Kota { get; set; }
+        public string NoTelpon { get; set; }
+        public string Email { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
+}
