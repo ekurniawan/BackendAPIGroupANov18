20712d632f496f5755e2d24fcb0c1e0e18df7763
ekurniawan <erick.kurniawan@gmail.com>
2018-11-19 10:57:39 +0700

    revisi appconfig

--- BackendKelasA/Startup.cs
+++ BackendKelasA/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BackendKelasA.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +26,11 @@ namespace BackendKelasA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //menambahkan Helper dan Dokter
+            services.AddScoped<IHelper, Helper>();
+            services.AddScoped<IDokter, DokterDAL>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
