347b2a84110984460ff06384651d44ee927f012a
ekurniawan <erick.kurniawan@gmail.com>
2018-11-19 10:04:04 +0700

    menambahkan dokter dal

---
+++ BackendKelasA/DAL/DokterDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace BackendKelasA.DAL
+{
+    public class DokterDAL : IDokter
+    {
+        public Task Delete(IDokter obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IDokter>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDokter> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(IDokter obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(IDokter obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
